40943a48fb99cf640df8dfecf401cb7fe37094ff
vivek-regmi-au5 <vivek3639@gmail.com>
2020-07-30 19:34:37 +0530

    changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const config = require("config");
+const path = require("path");
 const dbURI = config.get("mongoURI");
 const cors = require("cors");
 
