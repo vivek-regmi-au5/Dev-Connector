e9b5f3900d09171b015deb43095028be1be07592
vivek-regmi-au5 <vivek3639@gmail.com>
2020-07-30 19:30:11 +0530

    final

--- server.js
+++ server.js
@@ -29,6 +29,14 @@ app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/post", require("./routes/api/post"));
 
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 7777;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
